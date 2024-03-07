1729a41d2a5b100b231ce5137ee34349c0b80f48
Merced3 <cedgonzales@icloud.com>
2024-03-07 13:44:51 -0600

    edditing Homepage

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import useNavigate
+import React, { useState, useEffect } from 'react';
+import { jwtDecode as jwt_decode } from 'jwt-decode'; // Corrected import statement
+import { Link } from 'react-router-dom';
 import './HomePage.css';
 
 const HomePage = () => {
   const [isPanelOpen, setIsPanelOpen] = useState(false);
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = jwt_decode(token);
+      console.log('User ID from token:', decodedToken.id);
+      setUserId(decodedToken.id); // Store user ID from token
+    }
+  }, []);
 
   const togglePanel = () => {
     setIsPanelOpen(!isPanelOpen);
@@ -15,14 +26,13 @@ const HomePage = () => {
       {isPanelOpen && (
         <div className="navigationPanel">
           <Link to="/">Home</Link>
-          <Link to="/login">Login/Sign Up</Link>
+          {userId ? <Link to="/profile">Profile</Link> : <Link to="/login">Login/Sign Up</Link>}
           <Link to="/create-project">New Project</Link>
           <Link to="/projects">Saved Projects</Link>
         </div>
       )}
       <div className="centerButtons">
         <Link to="/create-project"><button>Create New Project</button></Link>
-        {/* Update this button to call goToProjectsPage on click */}
         <Link to="/projects"><button>View Your Projects</button></Link>
       </div>
     </div>
